1e78b02fcabdbc751b51027198b8fa0a8759c1f6
nguyen hiep <nguyenhuuhiep386@gmail.com>
2026-03-17 23:26:33 +0700

    Bai 4

---
+++ Bai4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Thông tin sinh viên
+        Console.WriteLine("Nguyen Huu Hiep");
+        Console.WriteLine("MSSV: 2415053122218");
+
+        List<int> numbers = new List<int>();
+
+        Console.Write("Nhap so phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap so: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        int count = 0;
+
+        foreach (int num in numbers)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("So luong so chan la: " + count);
+        Console.ReadKey();
+    }
+}
