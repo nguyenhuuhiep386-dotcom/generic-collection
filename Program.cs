7881dee9480ff05e561cc86990233b08a942601c
nguyen hiep <nguyenhuuhiep386@gmail.com>
2026-03-18 00:00:08 +0700

    Bai 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyen Huu Hiep - MSSV: 2415053122218");
+        Console.WriteLine();
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Nhap so: ");
+            numbers.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Danh sach vua nhap:");
+
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
