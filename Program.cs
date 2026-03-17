9efba0335fb7f2e1a6da54b7a64fb066a2eaaf6f
nguyen hiep <nguyenhuuhiep386@gmail.com>
2026-03-18 00:20:23 +0700

    Bai 12

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Class Student
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyen Huu Hiep - MSSV: 2415053122218");
+        Console.WriteLine();
+
+        List<Student> list = new List<Student>();
+
+        Console.Write("Nhap so sinh vien: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Student sv = new Student();
+
+            Console.Write("Nhap ID: ");
+            sv.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap ten: ");
+            sv.Name = Console.ReadLine();
+
+            list.Add(sv);
+        }
+
+        string name;
+        bool found;
+
+        while (true)
+        {
+            found = false;
+
+            Console.Write("Nhap ten can tim: ");
+            name = Console.ReadLine();
+
+            foreach (Student sv in list)
+            {
+                if (sv.Name == name)
+                {
+                    Console.WriteLine("Tim thay: ID = " + sv.Id + " - Ten = " + sv.Name);
+                    found = true;
+                }
+            }
+
+            if (found)
+                break;
+            else
+                Console.WriteLine("Khong tim thay, nhap lai!");
+            Console.ReadKey();
+        }
+    }
+}
