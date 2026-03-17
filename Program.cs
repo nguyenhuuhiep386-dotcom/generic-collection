891ae2cc57fdf7ad7255330ad9f39e1e2b3ceda3
nguyen hiep <nguyenhuuhiep386@gmail.com>
2026-03-18 00:19:15 +0700

    Bai 11

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Tao class Student
+class Student
+{
+    public int Id;
+    public string Name;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nguyen Huu Hiep - MSSV: 2415053122218");
+        Console.WriteLine();
+
+        List<Student> list = new List<Student>();
+
+        Console.Write("Nhap so sinh vien: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Student sv = new Student();
+
+            Console.Write("Nhap ID: ");
+            sv.Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhap ten: ");
+            sv.Name = Console.ReadLine();
+
+            list.Add(sv);
+        }
+
+        Console.WriteLine("Danh sach sinh vien:");
+
+        foreach (Student sv in list)
+        {
+            Console.WriteLine("ID: " + sv.Id + " - Ten: " + sv.Name);
+        }
+        Console.ReadKey();
+    }
+}
