2ba59a96ae99aad1e4e88a879ae35abb1edb7395
nguyen hiep <nguyenhuuhiep386@gmail.com>
2026-03-17 23:11:57 +0700

    Bai 2

---
+++ Bai2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Nguyen Huu Hiep");
+        Console.WriteLine("msv: 2415053122218");
+        Console.Write("Nhap so luong phan tu: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Nhap so: ");
+            int num = int.Parse(Console.ReadLine());
+            numbers.Add(num);
+        }
+
+        int sum = 0;
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        
+        Console.WriteLine("Tong = " + sum);
+        Console.ReadKey();
+    }
+}
